285d27c7397394176e580f5581795057ae4eafad
Nqv4 <109492317+NQV4@users.noreply.github.com>
2023-05-31 19:53:09 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 const HomePage = () => {
   const router = useRouter();
@@ -23,47 +24,53 @@ const HomePage = () => {
           alignItems: 'center',
           height: '100%',
           scrollSnapAlign: 'start',
-          backgroundImage: `url('images/1.png')`,
-          backgroundSize: '100% 100%',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
         }}
       >
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '3270px',  }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '3270px' }}>
           <button
-            style={{ width: '300px', height: '430px', marginRight: '110px', backgroundColor: 'transparent',
-            border: 'none' }}
+            style={{
+              width: '300px',
+              height: '430px',
+              marginRight: '110px',
+              backgroundColor: 'transparent',
+              border: 'none',
+            }}
             onClick={() =>
               handleButtonClick(
                 'https://discord.com/api/oauth2/authorize?client_id=1005064174138835004&permissions=8&scope=bot'
               )
             }
-          >
-            
-          </button>
+          ></button>
           <button
-            style={{ width: '300px', height: '430px', marginRight: '0px', backgroundColor: 'transparent',
-            border: 'none' }}
+            style={{
+              width: '300px',
+              height: '430px',
+              marginRight: '0px',
+              backgroundColor: 'transparent',
+              border: 'none',
+            }}
             onClick={() =>
               handleButtonClick(
                 'https://discord.com/api/oauth2/authorize?client_id=1109416272598405120&permissions=8&scope=bot'
               )
             }
-          >
-            
-          </button>
+          ></button>
           <button
-            style={{ width: '300px', height: '430px', marginLeft: '110px', backgroundColor: 'transparent',
-            border: 'none' }}
+            style={{
+              width: '300px',
+              height: '430px',
+              marginLeft: '110px',
+              backgroundColor: 'transparent',
+              border: 'none',
+            }}
             onClick={() =>
               handleButtonClick(
                 'https://discord.com/api/oauth2/authorize?client_id=1005342490917879908&permissions=8&scope=bot'
               )
             }
-          >
-            
-          </button>
+          ></button>
         </div>
+        <Image src="/images/1.png" alt="Background Image" layout="fill" objectFit="cover" />
       </div>
 
       <div
@@ -73,13 +80,10 @@ const HomePage = () => {
           alignItems: 'center',
           height: '100vh',
           scrollSnapAlign: 'start',
-          backgroundImage: `url('/images/2.png')`,
-          backgroundSize: '100% 100%',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
         }}
       >
         {/* 2番目の背景の内容 */}
+        <Image src="/images/2.png" alt="Background Image" layout="fill" objectFit="cover" />
       </div>
 
       <div
@@ -89,13 +93,10 @@ const HomePage = () => {
           alignItems: 'center',
           height: '100vh',
           scrollSnapAlign: 'start',
-          backgroundImage: `url('/images/3.png')`,
-          backgroundSize: '100% 100%',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
         }}
       >
         {/* 3番目の背景の内容 */}
+        <Image src="/images/3.png" alt="Background Image" layout="fill" objectFit="cover" />
       </div>
 
       <div
@@ -105,13 +106,10 @@ const HomePage = () => {
           alignItems: 'center',
           height: '100vh',
           scrollSnapAlign: 'start',
-          backgroundImage: `url('/images/4.png')`,
-          backgroundSize: '100% 100%',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
         }}
       >
         {/* 4番目の背景の内容 */}
+        <Image src="/images/4.png" alt="Background Image" layout="fill" objectFit="cover" />
       </div>
     </div>
   );
